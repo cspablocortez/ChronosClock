a663d414d5c0a85af898b7b6a928f5e1b5b403af
Pablo Cortez <cspablocortez@gmail.com>
2024-02-06 19:32:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,36 +8,21 @@ secondsSpan = document.getElementById("seconds");
 
 function formatTime() {
     const d = new Date();
-    let hours, minutes, seconds;
-    
-    if (d.getHours() < 10) {
-        hours = "0" + d.getHours();
-    } else {
-        hours = d.getHours();
-    }
-    
-    if (d.getMinutes() < 10) {
-        minutes = "0" + d.getMinutes();
-    } else {
-        minutes = d.getMinutes();
-    }
-    
-    if (d.getSeconds() < 10) {
-        seconds = "0" + d.getSeconds();
-    } else {
-        seconds = d.getSeconds();
-    }
+    const hours = formatDigit(d.getHours());
+    const minutes = formatDigit(d.getMinutes());
+    const seconds = formatDigit(d.getSeconds());
 
     // 12 hour clock
-    if (hours > 12) {
-        hours = hours % 12;
-        return hours + "|" + minutes + "|" + seconds + " PM";
-    } else {
-        return hours + "|" + minutes + "|" + seconds + " AM";
-    }
-    
-    return hours + ":" + minutes + ":" + seconds;
+    let timeString = hours + "|" + minutes + "|" + seconds;
+    timeString += (hours >= 12) ? " PM" : " AM";
+
+    return timeString;
 }
+
+function formatDigit(digit) {
+    return digit < 10 ? "0" + digit : digit;
+}
+
 
 function setTime() {
     const timeCell = document.getElementById('time');
